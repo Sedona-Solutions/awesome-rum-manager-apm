0a564ce8e9ede2b25effd06dd44530c4cdf6c767
camboui <mbranger@sedona.fr>
2023-01-17 15:06:37 +0100

    frontend : github pages configuration

--- awesome-front/svelte.config.js
+++ awesome-front/svelte.config.js
@@ -1,5 +1,6 @@
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
+const dev = process.argv.includes('dev');
 
 export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
@@ -13,7 +14,13 @@ export default {
             assets: 'build',
             fallback: null,
             precompress: false,
-            strict: true
+            strict: true,
+            paths: {
+                base: dev ? '' : '/awesome-rum-manager-apm',
+            },
+            // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+            // For example, instead of '_app', use 'app_', 'internal', etc.
+            appDir: 'internal',
         }),
         alias: {
             '@awesome': 'src'
