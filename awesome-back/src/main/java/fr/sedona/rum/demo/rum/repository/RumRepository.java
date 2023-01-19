3565dd2d6b80bdcef47c5d12987a1aab6234dcaf
Denis Cucchietti <dcucchietti@sedona.fr>
2023-01-19 14:37:00 +0100

    FIX: typo, remove h

--- awesome-back/src/main/java/fr/sedona/rum/demo/rum/repository/RumRepository.java
+++ awesome-back/src/main/java/fr/sedona/rum/demo/rum/repository/RumRepository.java
@@ -6,7 +6,7 @@ import fr.sedona.rum.demo.rum.model.domain.RumEntity;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 /**
- * Rhum repository
+ * Rum repository
  */
 @ApplicationScoped
 public class RumRepository implements PanacheRepository<RumEntity> {
