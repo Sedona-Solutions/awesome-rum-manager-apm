f8394ad683e8f38e57f6e6d2e1dc28baedad3d11
mbroutin <mbroutin@sedona.fr>
2023-01-30 14:34:29 +0100

    fix(pages): use proper context for github pages

--- awesome-front/svelte.config.js
+++ awesome-front/svelte.config.js
@@ -1,4 +1,4 @@
-import {vitePreprocess} from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
 const config = {
@@ -6,22 +6,15 @@ const config = {
     // for more information about preprocessors
     preprocess: vitePreprocess(),
     kit: {
+        // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+        // For example, instead of '_app', use 'app_', 'internal', etc.
+        appDir: 'internal',
         adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
             fallback: '404.html',
-            precompress: false,
-            strict: true,
-            paths: {
-                base: '/awesome-rum-manager-apm',
-                assets:  '/awesome-rum-manager-apm',
-            },
-            // If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-            // For example, instead of '_app', use 'app_', 'internal', etc.
-            appDir: 'internal',
         }),
+        paths: {
+            base: '/awesome-rum-manager-apm',
+        },
         alias: {
             '@awesome': 'src'
         }
